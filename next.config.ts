96269c4054d7f4315c9c142f92784311b8ca818d
vivivihadi <oliynykvika07@gmail.com>
2026-09-18 21:00:06 +0300

    fix4

--- next.config.ts
+++ next.config.ts
@@ -3,18 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-
-images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-        port: "",
-        pathname: "/**",
-      },
-    ],
-},
 };
 
-
-export default nextConfig; 
+export default nextConfig;
